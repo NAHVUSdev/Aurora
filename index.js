6439d82f54381e02dbd7844bc2cd24fe5c059b2c
nahvusdev <entertrenton@outlook.com>
2020-03-05 11:32:35 -0600

    setup

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('Server Error 500');
 }).listen(8080);
