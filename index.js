71d62df43edb161dca02918f0965642004fb2e50
nahvusdev <entertrenton@outlook.com>
2020-03-05 11:57:50 -0600

    added comming soon page

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ app.get('/', function (req, res) {
     "use strict";
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 });
+app.get('/global.css', function (req, res) {
+    "use strict";
+    res.sendFile('global.css', {root: path.join(__dirname, 'public/css')});
+});
+app.get('/default.js', function (req, res) {
+    "use strict";
+    res.sendFile('default.js', {root: path.join(__dirname, 'public/javascript')});
+});
 
 app.listen(8080);
